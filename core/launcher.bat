323b56c17bc36b8a794c4d7007b8277ab092e3e0
MininMobile <notminin@gmail.com>
2018-07-04 11:46:48 +0200

    fix launcher_new_create

--- core/launcher.bat
+++ core/launcher.bat
@@ -1,50 +1,53 @@
 @echo off
 :launcher_menu
-	cls
-	echo SOLID ADVENTURE EDITOR
-	echo.
-	echo.
-	echo 1) Create New Project
-	echo 2) Load Existing Project
-	echo.
-	echo 0) Exit
-	echo.
-	echo.
-	set /p input=^> 
+cls
+echo SOLID ADVENTURE EDITOR
+echo.
+echo.
+echo 1) Create New Project
+echo 2) Load Existing Project
+echo.
+echo 0) Exit
+echo.
+echo.
+set /p input=^> 
 
-	if "%input%" == "1" goto launcher_new
-	if "%input%" == "2" goto launcher_load
-	if "%input%" == "0" goto eof
+if "%input%" == "1" goto launcher_new
+if "%input%" == "2" goto launcher_load
+if "%input%" == "0" goto eof
 goto launcher_menu
 
 :launcher_new
-	cls
-	echo NEW PROJECT
-	echo.
-	echo.
-	echo type 'exit' anytime to cancel
-	echo.
-	set /p project=Project Name^> 
-	if "%project%" == "exit" goto launcher_menu
-	echo.
-	set /p input=Create new project '%project%'? [Y/N]^> 
-	if "%input%" == "Y" goto launcher_new_create
-	if "%input%" == "y" goto launcher_new_create
-	goto launcher_menu
+cls
+echo NEW PROJECT
+echo.
+echo.
+echo type 'exit' anytime to cancel
+echo.
+set /p project=Project Name^> 
+if "%project%" == "exit" goto launcher_menu
+echo.
+set /p input=Create new project '%project%'? [Y/N]^> 
+if "%input%" == "Y" goto launcher_new_create
+if "%input%" == "y" goto launcher_new_create
+goto launcher_menu
 goto eof
 
 :launcher_new_create
-	if "%project%" == "" goto launcher_menu
+if "%project%" == "" goto launcher_menu
 
-	mkdir saves/%project%
-	cd saves/%project%
+mkdir saves
+cd saves
+mkdir %project%
+cd %project%
 
-	echo @echo off>%project%.bat
-	echo call core.bat>>%project%.bat
-	
-	cd ../..
+echo @echo off>%project%.bat
+echo call core.bat>>%project%.bat
 
-	goto launcher_menu
+cd ..
+cd ..
+
+goto launcher_menu
 goto eof
 
 :launcher_load
