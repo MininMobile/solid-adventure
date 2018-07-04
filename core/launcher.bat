f6bba74e1f1c2de556c0ea1b92e4339f912d7edb
MininMobile <notminin@gmail.com>
2018-07-04 10:52:43 +0200

    add launcher menu

--- core/launcher.bat
+++ core/launcher.bat
@@ -1,2 +1,28 @@
 @echo off
-pause
+:launcher_menu
+cls
+echo SOLID ADVENTURE EDITOR
+echo.
+echo.
+echo 1) Create New Project
+echo 2) Load Existing Project
+echo.
+echo 0) Exit
+echo.
+echo.
+set /p input=^>
+
+if "%input%" == "1" goto launcher_new
+if "%input%" == "2" goto launcher_load
+if "%input%" == "0" goto eof
+goto launcher_menu
+
+:launcher_new
+
+goto eof
+
+:launcher_load
+
+goto eof
+
+:eof
